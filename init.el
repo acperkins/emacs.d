5f50afe2c07ed32fd4c05df70b318188536515ec
Anthony Perkins <anthony@acperkins.com>
2017-03-27 08:33:34 +0000

    Initial config import

---
+++ init.el
@@ -0,0 +1,42 @@
+(require 'package)
+(add-to-list 'package-archives '("org"
+  . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa"
+  . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable"
+  . "http://stable.melpa.org/packages/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+ (require 'use-package))
+
+(use-package markdown-mode
+  :ensure t)
+(use-package helm
+  :ensure t)
+
+(setq c-default-style "bsd")
+(c-set-offset 'arglist-cont-nonempty '4)
+(setq inhibit-startup-screen t)
+(setq backup-inhibited t)
+(setq auto-save-default nil)
+(setq initial-major-mode 'org-mode)
+(load-theme 'tango-dark)
+
+;; Keep these at the end of the file and do not edit them by hand.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
