1789228b100164dc671c46a777fa100c5668ddb4
Anthony Perkins <anthony@acperkins.com>
2017-03-31 10:58:49 +0000

    Add a batch file to start Emacs in server mode

---
+++ server.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+REM Start Emacs in daemon mode on Windows. It assumes Emacs is
+REM installed into %LOCALAPPDATA\Emacs.
+TITLE Emacs daemon
+"%LOCALAPPDATA%\Emacs\bin\runemacs.exe" --daemon --chdir %USERPROFILE%
