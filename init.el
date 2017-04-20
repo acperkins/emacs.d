22409afa7c8dd4abc0156cea19fa89e7a8a8ec82
Anthony Perkins <anthony@acperkins.com>
2017-04-20 09:45:18 +0000

    Log completion time on task close

--- init.el
+++ init.el
@@ -80,6 +80,7 @@
 (setq initial-major-mode 'org-mode)
 (setq line-move-visual nil)
 (setq org-startup-folded nil)
+(setq org-log-done 'time)
 (setq shr-inhibit-images t)
 (setq shr-use-fonts nil)
 (setq track-eol t)
