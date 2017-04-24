b7b229daa561fefb5aa22a838a5d643f9c4a1cd8
Anthony Perkins <anthony@acperkins.com>
2017-04-24 08:31:07 +0000

    Stop using whitespace mode

--- init.el
+++ init.el
@@ -90,8 +90,6 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cc" 'org-capture)
 (global-linum-mode t)
-(global-whitespace-mode t)
-(global-whitespace-toggle-options 'newline)
 (load-theme 'tango-dark)
 (setq auto-save-default nil)
 (setq backup-inhibited t)
