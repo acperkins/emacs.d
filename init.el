d995b8dfc55a8cad2401bbf0458cc704e1bceff6
Anthony Perkins <anthony@acperkins.com>
2017-11-01 14:10:30 +0000

    Do not use helm-find-files

--- init.el
+++ init.el
@@ -28,7 +28,6 @@
     :ensure t
     :init
     (global-set-key (kbd "M-x") 'helm-M-x)
-    (global-set-key (kbd "C-x C-f") 'helm-find-files)
     :config
     (require 'helm-config)
     (helm-mode t)))
