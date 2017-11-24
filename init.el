d7b3a5cc3baff9ec92687f1cad88e5408562cfc8
Anthony Perkins <anthony@acperkins.com>
2017-11-24 12:12:11 +0000

    Enable helm-find-files

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
     :ensure t
     :init
     (global-set-key (kbd "M-x") 'helm-M-x)
+    (global-set-key (kbd "C-x C-f") 'helm-find-files)
     :config
     (require 'helm-config)
     (helm-mode t)))
