820bf47e0242a325de5f501a5a90de8351a15642
Anthony Perkins <anthony@acperkins.com>
2017-11-26 00:32:47 +0000

    Add GTD

--- init.el
+++ init.el
@@ -106,6 +106,40 @@ as input."
 (setq auto-save-file-name-transforms
       `((".*" ,user-temporary-file-directory t)))
 
+;; Getting Things Done in org-mode.
+;; Source: <https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html>
+(setq org-agenda-files '("~/Sync/gtd/inbox.org"
+                         "~/Sync/gtd/projects.org"
+                         "~/Sync/gtd/reminders.org"))
+(setq org-capture-templates '(("t" "Todo [inbox]" entry
+                               (file+headline "~/Sync/gtd/inbox.org" "Tasks")
+                               "* TODO %i%?")
+                              ("r" "Reminder" entry
+                               (file+headline "~/Sync/gtd/reminders.org" "Reminder")
+                               "* %i%? \n %U")))
+(setq org-refile-targets '(("~/Sync/gtd/inbox.org" :maxlevel . 3)
+                           ("~/Sync/gtd/someday.org" :level . 1)
+                           ("~/Sync/gtd/reminders.org" :maxlevel . 2)))
+(setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+(setq org-agenda-custom-commands 
+      '(("w" "Work" tags-todo "@work"
+         ((org-agenda-overriding-header "Work")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))))
+(defun my-org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
+(defun org-current-is-todo ()
+  (string= "TODO" (org-get-todo-state)))
+
 ;; Main options that don't come under other sections.
 (c-set-offset 'arglist-cont-nonempty '4)
 (define-key global-map "\C-ca" 'org-agenda)
