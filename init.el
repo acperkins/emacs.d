c24eb93df5e96311fb9fe49c7ddf44c584d6c777
Anthony Perkins <anthony@acperkins.com>
2017-11-27 23:23:08 +0000

    Slim down the package sources in use

--- init.el
+++ init.el
@@ -4,12 +4,8 @@
 
 ;; Set up the Emacs packaging system.
 (require 'package)
-(add-to-list 'package-archives '("melpa"
-  . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable"
   . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("org"
-  . "http://orgmode.org/elpa/"))
 (setq package-enable-at-startup nil)
 (package-initialize)
 (unless (package-installed-p 'use-package)
