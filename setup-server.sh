d1b709fc86be5ca776d3e35b4999bf66407a7815
Anthony Perkins <anthony@acperkins.com>
2018-08-02 09:06:02 +0100

    Switch from systemd to autostart

--- setup-server.sh
+++ setup-server.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 APPLICATION_DIR=~/.local/share/applications
-SYSTEMD_SERVICE_DIR=~/.config/systemd/user
+AUTOSTART_DIR=~/.config/systemd/user
 
 mkdir -p $APPLICATION_DIR
 cp emacs.desktop $APPLICATION_DIR/emacs.desktop
-mkdir -p $SYSTEMD_SERVICE_DIR
-cp emacs.service $SYSTEMD_SERVICE_DIR/emacs.service
-
-systemctl --user daemon-reload && \
-    systemctl --user start emacs.service && \
-    systemctl --user enable emacs.service
+mkdir -p $AUTOSTART_DIR
+cp emacs-server.desktop $AUTOSTART_DIR/emacs.service
+emacs --daemon
