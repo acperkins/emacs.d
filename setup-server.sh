ea876aafc01a86887b6d1fd497f228cae6a18ef9
Anthony Perkins <anthony@acperkins.com>
2018-08-19 18:22:37 +0100

    Fix Emacs setup

--- setup-server.sh
+++ setup-server.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 APPLICATION_DIR=~/.local/share/applications
-AUTOSTART_DIR=~/.config/systemd/user
+AUTOSTART_DIR=~/.config/autostart
 
 mkdir -p $APPLICATION_DIR
 cp emacs.desktop $APPLICATION_DIR/emacs.desktop
 mkdir -p $AUTOSTART_DIR
-cp emacs-server.desktop $AUTOSTART_DIR/emacs.service
+cp emacs-server.desktop $AUTOSTART_DIR/emacs.desktop
 emacs --daemon
