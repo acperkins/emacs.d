f4d6fba0bea371f55f59226b86d1e933b71ec2e4
Anthony Perkins <anthony@acperkins.com>
2020-06-04 16:10:07 +0100

    Add dpkg-dev-el package

---
+++ elpa/debian-el-37.4/debian-el-pkg.el
@@ -0,0 +1,4 @@
+(define-package "debian-el" "37.4" "Emacs helpers specific to Debian users" 'nil :commit "9690c4adb71e0fc7d00fea24b49ba944f913f4f5")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/dpkg-dev-el-37.1/dpkg-dev-el-pkg.el
@@ -0,0 +1,6 @@
+(define-package "dpkg-dev-el" "37.1" "Emacs modes for debian packaging"
+  '((debian-el "37"))
+  :commit "54b137309b078ec39f4b982368c500df04d4bd5f")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
