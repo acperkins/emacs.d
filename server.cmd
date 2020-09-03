752b85af62cbbb9a262ebc5b2a5d6ec2623feebf
Anthony Perkins <anthony.perkins@mkcollege.ac.uk>
2020-09-03 10:17:50 +0100

    Start Emacs in %SystemRoot%\System32

--- server.cmd
+++ server.cmd
@@ -2,4 +2,4 @@
 REM Start Emacs in daemon mode on Windows. It assumes Emacs is
 REM installed into C:\Program Files\Emacs\x86_64.
 TITLE Emacs daemon
-"C:\Program Files\Emacs\x86_64\bin\runemacs.exe" --daemon --chdir %USERPROFILE%
+"C:\Program Files\Emacs\x86_64\bin\runemacs.exe" --daemon --chdir %SystemRoot%\System32
